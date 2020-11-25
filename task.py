0e0bb35f439c2a0eed2f7617f0c94ce253f860d3
KapralDaun <64838965+KapralDaun@users.noreply.github.com>
2020-11-25 16:49:37 +0300

    Update task.py

--- task.py
+++ task.py
@@ -1,4 +1,3 @@
-
 class Solution:
     add = {
         'I': 1,
@@ -19,10 +18,10 @@ class Solution:
         ('C', 'M'): 800,
     }
 
-    def roman_to_arabic(self, roman_number):
+    def romanToInt(self, s: str) -> int:
         result = 0
         prev_char = None
-        for char in roman_number:
+        for char in s:
             if prev_char and self.add[prev_char] < self.add[char]:
                 result += self.div[(prev_char, char)]
             else:
@@ -31,52 +30,7 @@ class Solution:
         return result
 
 
-class UnitTests(Solution):
-    def all_tests(self):
-        if self.roman_to_arabic('I') != 1:
-            print('I is not 1')
-
-        if self.roman_to_arabic('III') != 3:
-            print('III is not 3')
-
-        if self.roman_to_arabic('IV') != 4:
-            print('IV is not 4')
-
-        if self.roman_to_arabic('IX') != 9:
-            print('IX is not 9')
-
-        if self.roman_to_arabic('XXX') != 30:
-            print('XXX is not 30')
-
-        if self.roman_to_arabic('LVIII') != 58:
-            print('LVIII is not 58')
-
-        if self.roman_to_arabic('CCXXVIII') != 228:
-            print('CCXXVIII is not 228')
-
-        if self.roman_to_arabic('DLXXXV') != 585:
-            print('DLXXXV is not 585')
-
-        if self.roman_to_arabic('DCCCLXIV') != 864:
-            print('DCCCLXIV is not 864')
-
-        if self.roman_to_arabic('MD') != 1500:
-            print('MD is not 1500')
-
-        if self.roman_to_arabic('MCMXCIV') != 1994:
-            print('MCMXCIV is not 1994')
-
-        if self.roman_to_arabic('MM') != 2000:
-            print('MM is not 2000')
-
-        if self.roman_to_arabic('MMDCXLVIII') != 2648:
-            print('MMDCXLVIII is not 2648')
-
-
 def main():
-    y = UnitTests()
-    y.all_tests()
-    Solution.roman_to_arabic(1,'MMDCXLVIII')
     pass
 
 
